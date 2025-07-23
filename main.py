2d52662c9b913fd09e25c38e522db7c3f3e35852
MitchellBarure <m.barure@alustudent.com>
2025-07-23 17:06:06 +0200

    First draft of script complete

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+#Import in-built String module to handle punctuation in essays
+import string
+
+"""This function reads the content of a file and returns it as a string allowing us to analyze the file content with the detector"""
 def read_file(file_path):
-    """This function reads the content of a file and returns it as a string allowing us to analyze the file content with the detector"""
     try:
         with open(file_path, 'r') as file:  #Opens the file in read mode
             return file.read()
@@ -7,11 +10,8 @@ def read_file(file_path):
         print(f"File not found:{file_path}")
         return ""
 
-#Import String function to handle punctuation in essays
-import string
-
+"""Converts text to lowercase, removes punctuation and spilts essay into words"""
 def preprocess(text):
-    """Converts text to lowercase, removes punctuation and spilts essay into words"""
     #Convert to lowercase
     text = text.lower()
 
@@ -22,6 +22,48 @@ def preprocess(text):
     words = text.split()
 
     return words  
+
+#Returns a lists of words that are found in the essays being compared
+def find_common_words(words1, words2):
+    #Convert both lists to sets to remove duplicates and allow easy comparison
+    set1 = set(words1)
+    set2 = set(words2)
+
+    #Find common words
+    common = set1.intersection(set2)
+
+    return list(common)
+
+#Calculates the Plagiarism percentage between two essays
+def calculate_percentage_plagiarism(words1, words2):
+    """Define variables again although they are in def find_common_words because functions have a local scope"""
+    set1 = set(words1)
+    set2 = set(words2)
+    
+    if not set1 and not set2: #Prevents a math error (division by zero) if the number of unique words=0
+        return 0.0
+    
+    intersection = set1.intersection(set2)
+    union = set1.union(set2)
+
+    percentage = (len(intersection) / len(union)) * 100
+    return round(percentage, 2)
+
+#Allows user to search for a specific word in essay1 and essay2
+def search_word(word, words1, words2):
+    count1 = words1.count(word)
+    count2 = words2.count(word)
+
+    if count1 == 0 and count2 == 0:
+        print(f"The word '{word}' was not found in either essay.")
+        return False
+    else:
+        print(f"Results for '{word}:'")
+        if count1 > 0:
+            print(f"Essay 1: {count1} occurrence(s)")
+        if count2 > 0:
+            print(f"Essay 2: {count2} occurrrence(s)")
+            return True
 
 #Defining paths to the essays
 essay1_path = "essay_samples/essay1.txt"
@@ -38,9 +80,21 @@ print("Essay 2 preview:", essay2[:100])"""
 essay1_words = preprocess(essay1)
 essay2_words = preprocess(essay2)
 
-#Confirm that the preprocess function was successful
+"""#Confirm that the preprocess function was successful
 print("Processed Essay 1 Words:", essay1_words[:10])
-print("Processed Essay 2 Words:", essay2_words[:10])
+print("Processed Essay 2 Words:", essay2_words[:10])"""
+
+#Find and print common words
+common_words = find_common_words(essay1_words, essay2_words)
+print(f"Common words ({len(common_words)} found):", common_words)
+
+#Calculate the plagiarism percentage
+plagiarism_percent = calculate_percentage_plagiarism(essay1_words, essay2_words)
+print(f"Plagiarism detected: {plagiarism_percent}%")
+
+#Ask user to search for a specific word
+word_to_search = input("\n Enter a word to search for in  both essays:").lower()
+search_word(word_to_search, essay1_words, essay2_words)
 
 
 
